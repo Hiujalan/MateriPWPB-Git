1ad6245668e0e9e1821fffecefc99518a6d489eb
Hiujalan <117693727+Hiujalan@users.noreply.github.com>
2023-07-28 09:05:34 +0700

    Update MainActivity.java

--- HelloConstraint/app/src/main/java/com/hiujalan/helloconstraint/MainActivity.java
+++ HelloConstraint/app/src/main/java/com/hiujalan/helloconstraint/MainActivity.java
@@ -9,7 +9,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // Membuat Variabel mCount dengan niali 0
     private int mCount = 0;
+    // memuat referensi TextView untuk di masukkan dalam variabel mShowCount
     private TextView mShowCount;
 
     @Override
@@ -20,12 +22,14 @@ public class MainActivity extends AppCompatActivity {
         mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
+    // Untuk menampilkan popup message Hello Toast
     public void showToast(View view) {
         Context context = getApplicationContext();
         Toast toast = Toast.makeText(context, R.string.message_toast, Toast.LENGTH_LONG);
         toast.show();
     }
 
+    // Untuk menampilkan angka yang telah di tambah
     public void countUp(View view) {
         mCount++;
         if (mCount <= 10) {
@@ -35,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // Untuk mereset Nilai mCount menjadi 0
     public void reset(View view) {
         mCount = 0;
         if (mCount == 0) {
@@ -44,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // untuk mengurangi Nilai mCount
     public void unCount(View view) {
         mCount--;
         if (mCount >= -10) {
