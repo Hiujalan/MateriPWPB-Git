43304842ba01d0c50ea4185e0c562a5a4eb34b0a
Hiujalan <117693727+Hiujalan@users.noreply.github.com>
2023-07-28 09:01:39 +0700

    Update MainActivity.java

--- AlanHelloRelative/app/src/main/java/com/hiujalan/alanhellorelative/MainActivity.java
+++ AlanHelloRelative/app/src/main/java/com/hiujalan/alanhellorelative/MainActivity.java
@@ -9,7 +9,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    // Membuat Variabel mCount dengan niali 0
     private int mCount = 0;
+    // memuat referensi TextView untuk di masukkan dalam variabel mShowCount
     private TextView mShowCount;
 
     @Override
@@ -20,12 +22,14 @@ public class MainActivity extends AppCompatActivity {
         mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
+    // Untuk menampilkan popup message Hello Toast
     public void showToast(View view) {
         Context context = getApplicationContext();
         Toast toast = Toast.makeText(context, R.string.message_toast, Toast.LENGTH_LONG);
         toast.show();
     }
 
+    // Untuk menampilkan angka yang telah di tambah
     public void countUp(View view) {
         mCount++;
         if (mCount <= 10) {
