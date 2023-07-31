9aa7b4571400f4545e55aff3f8e4138fe741ac60
Hiujalan <117693727+Hiujalan@users.noreply.github.com>
2023-07-31 11:45:35 +0700

    Update MainActivity.java

--- AlanAplication1/app/src/main/java/com/hiujalan/alanaplication1/MainActivity.java
+++ AlanAplication1/app/src/main/java/com/hiujalan/alanaplication1/MainActivity.java
@@ -30,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Untuk menampilkan angka yang telah di tambah
-    public void countUp(View view) {
-        mCount++;
+    public void countUp(View view) { 
         if (mCount <= 10) {
+             mCount++;
             if (mShowCount != null) {
                 mShowCount.setText(Integer.toString(mCount));
             }
